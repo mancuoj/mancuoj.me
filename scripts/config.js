d31f3a5662c45468ed78033758e5b6adccc45156
mancuoj <1337381768@qq.com>
2023-03-28 16:57:14 +0800

    social links

---
+++ scripts/config.js
@@ -0,0 +1,43 @@
+export const links = [
+  {
+    text: '',
+    icon: 'github.svg',
+    url: 'https://github.com/mancuoj'
+  },
+  {
+    text: '',
+    icon: 'csdn.svg',
+    url: 'https://blog.csdn.net/Mancuojie'
+  },
+  {
+    text: '',
+    icon: 'juejin.svg',
+    url: 'https://juejin.cn/user/3466105460624760'
+  },
+  {
+    text: '',
+    icon: 'zhihu.svg',
+    url: 'https://www.zhihu.com/people/clown-95-18'
+  },
+  {
+    text: '',
+    icon: 'bilibili.svg',
+    url: 'https://space.bilibili.com/44805047'
+  }
+]
+
+export const projects = [
+  {
+    title: '',
+    description: '',
+    url: '',
+    logo: ''
+  }
+]
+
+export const posts = [
+  {
+    title: '',
+    date: ''
+  }
+]
