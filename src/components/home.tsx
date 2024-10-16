571b46d4f3efbb6d331f142211a6e2bed54e494f
mancuoj <mancuoj@gmail.com>
2024-10-16 18:29:41 +0800

    chore: update

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,7 @@
 export function Home() {
   return (
     <div className="font-sans antialiased">
-      <div className="flex min-h-dvh flex-col items-center justify-center gap-5">
+      <div className="flex min-h-dvh flex-col items-center justify-center gap-5 dark:bg-gray-900 dark:text-white">
         <h1 className="font-serif text-3xl font-bold">Hi, I'm mancuoj</h1>
         <div className="flex items-center gap-2 text-2xl">
           <a href="https://github.com/mancuoj" className="i-lucide-github" />
